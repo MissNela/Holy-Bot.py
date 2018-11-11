76524329c7fd39bedf3d1fd196509ab232c37e19
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-11 12:16:13 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -30,21 +30,21 @@ async def on_ready():
 
 @bot.command()
 async def dev():
-         embed = discord.Embed(
-		 title = "Bot developer"
-		 description = "@Kazuto Kirigaya#8757 is the owner...... also DM him about any command suggestions or if any extra help is needed"
-         colour = discord.Colour.blue()	
+    embed = discord.Embed(
+	title = "Bot developer"
+        description = "@Kazuto Kirigaya#8757 is the owner...... also DM him about any command suggestions or if any extra help is needed"
+    colour = discord.Colour.blue()	
 ) 
-         await bot.say(embed=embed)
+    await bot.say(embed=embed)
 		 
 @bot.command()
 async def CoO():
-         embed = discord.Embed(
-		 title = "Co-Owner"
-		 description = "Co-Owner of server is Mrs.Nela"
-         colour = discord.Colour.dark_blue()
+    embed = discord.Embed(
+	title = "Co-Owner"
+	description = "Co-Owner of server is Mrs.Nela"
+     colour = discord.Colour.dark_blue()
 )
-         await bot.say(embed=embed)
+     await bot.say(embed=embed)
 		
 @bot.command()
 async def SCP():
