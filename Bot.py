9a9d7245f527abd533e391234c7d5d7a925859e7
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-17 21:12:20 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -21,7 +21,7 @@ from discord import Game, Embed, Color, Status, ChannelType
 
 bot=commands.Bot(command_prefix='h!')
 
-@bot.event
+@client.event
 async def on_ready():
 	print('The bot is ready!')
 	print(bot.user.name)
@@ -33,25 +33,25 @@ async def dev():
     
 
          
-   await bot.say("@Kazuto Kirigaya#8757 is the owner...... also DM him about any command suggestions or if any extra help is needed")
+   await client.say("@Kazuto Kirigaya#8757 is the owner...... also DM him about any command suggestions or if any extra help is needed")
 		 
 @client.command(pass_context=True)
 async def CoO():
     
-    await bot.say("Co-Owner of server is Mrs.Nela!")
+    await client.say("Co-Owner of server is Mrs.Nela!")
 	
 		
-@bot.command()
+@client.command()
 async def SCP():
-	await bot.say('well want info on the foundation i think the link is Https://SCP-wiki.com i will update this command if needed')
+	await client.say('well want info on the foundation i think the link is Https://SCP-wiki.com i will update this command if needed')
 	
-@bot.command()
+@client.command()
 async def suggestion():
-	await bot.say('ok DM my owner Kazuto Kirigaya#8757 and say i have a suggestion and i he will get to you when he can')
+	await client.say('ok DM my owner Kazuto Kirigaya#8757 and say i have a suggestion and i he will get to you when he can')
 	
-@bot.command()
+@client.command()
 async def extra():
-	await bot.say('same with the h!suggestion command DM my owner Kazuto Kirigaya#8757 and say i need help and he will get to you when he can')
+	await client.say('same with the h!suggestion command DM my owner Kazuto Kirigaya#8757 and say i need help and he will get to you when he can')
 
 bot.run(os.getenv('BOT_TOKEN'))
 
